e86996b09c512752a9600879c33e0df3748214a9
Nicole Gay <nicolerg@stanford.edu>
2022-07-22 09:43:16 -0700

    fix make_json_singleton.sh

--- src/make_json_singleton.sh
+++ src/make_json_singleton.sh
@@ -11,7 +11,8 @@ json_dir=$3
 
 mkdir -p ${json_dir}
 
-samples=$(ls $fastq_dir | grep "fastq.gz" | grep "L0" | grep -v "Undetermined" | grep -E "R1|R2" | sed "s/_L00.*//" | sort | uniq)
+samples=$(ls $fastq_dir | grep "fastq.gz" | grep -v "Undetermined" | grep -E "R1|R2" | sed "s/_R.*//" | sort | uniq)
+echo $samples
 
 for i in $samples; do
 
@@ -19,7 +20,7 @@ for i in $samples; do
     json_file=${json_dir}/${i}.json
 
     echo "{" > ${json_file}
-    echo "    \"atac.title\" : \"${i}\"," >> ${json_file}
+    echo "    \"atac.description\" : \"${i}\"," >> ${json_file}
 
     # standard parameters for this project
     cat ${json_base} >> ${json_file}
@@ -27,9 +28,9 @@ for i in $samples; do
     # add paths to FASTQ files 
     echo "    \"atac.fastqs_rep1_R1\" : [" >> ${json_file}
     
-    lanes=$(ls ${fastq_dir} | grep "$i" | grep "L00" | grep "R1" | wc -l)
+    lanes=$(ls ${fastq_dir} | grep "$i" | grep "R1" | wc -l)
     counter=1
-    for r1 in $(ls ${fastq_dir} | grep "$i" | grep "L00" | grep "R1"); do
+    for r1 in $(ls ${fastq_dir} | grep "$i" | grep "R1"); do
         if [ "$counter" = "$lanes" ]; then
             echo "        \"${fastq_dir}/${r1}\"" >> ${json_file}
         else
@@ -43,7 +44,7 @@ for i in $samples; do
     echo "    \"atac.fastqs_rep1_R2\" : [" >> ${json_file}
 
     counter=1
-    for r2 in $(ls ${fastq_dir} | grep "$i" | grep "L00" | grep "R2"); do
+    for r2 in $(ls ${fastq_dir} | grep "$i" | grep "R2"); do
         if [ "$counter" = "$lanes" ]; then
             echo "        \"${fastq_dir}/${r2}\"" >> ${json_file}
         else
