f1c252143eb3d8a1d8074031edcafb91c147204d
Mihir Samdarshi <msamdars@stanford.edu>
2022-09-20 14:58:58 -0700

    feat: fix wrapper script with better outputs

--- src/atac-seq-human-wrapper.sh
+++ src/atac-seq-human-wrapper.sh
@@ -15,6 +15,7 @@ GCS_BUCKET=mihir-test
 FASTQ_RAW_DIR="gs://mihir-test/atac-seq/batch1_20220710/fastq_raw"
 ATAC_OUTPUT_DIR="gs://mihir-test/atac-seq/batch1_20220710/processed"
 BASE_JSON="batch1_20220710_base.json"
+SAMPLE_METADATA_FILENAME="batch1_20220710_sample_metadata.csv"
 # set this to the region you want to run in (e.g. us-central1), the region should be compatible with life sciences API
 CROMWELL_OUT_DIR="gs://mihir-test/pipelines/out"
 # you need to have a service account with access to the life sciences API on the VM
@@ -25,6 +26,9 @@ REMOTE_KEY_FILE=~/.keys/service_account_key.json
 JSON_DIR=json/
 OUT_DIR=outputs/
 MOUNT_DIR=~/mnt/
+
+#
+LOCAL_ATAC_OUT_DIR=${ATAC_OUTPUT_DIR#"gs://"}
 
 #### Script ####
 if [ ! "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
@@ -93,28 +97,27 @@ python3 src/get_execution_status.py $OUT_DIR/"$BATCH_PREFIX"_submissions.json
 echo "Downloading outputs..."
 bash src/croo.sh $OUT_DIR/"$BATCH_PREFIX"_submissions.json $CROMWELL_OUT_DIR/atac/ $ATAC_OUTPUT_DIR/croo true
 
+echo "Mounting GCS bucket..."
+mkdir -p "$MOUNT_DIR"/"$GCS_BUCKET"
+gcsfuse --implicit-dirs "$GCS_BUCKET" "$MOUNT_DIR"/"$GCS_BUCKET"
+
 #get qc tsv report
-echo "Creating qc2tsv report"
-bash src/qc2tsv.sh $ATAC_OUTPUT_DIR/croo "$BATCH_PREFIX"_qc.tsv
+echo "Creating qc2tsv report..."
+mkdir -p "$MOUNT_DIR"/"$GCS_BUCKET"/qc/
+bash src/qc2tsv.sh $ATAC_OUTPUT_DIR/croo "$MOUNT_DIR"/"$GCS_BUCKET"/qc/"$BATCH_PREFIX"_qc.tsv
 
 # reorganize croo outputs for quantification
-echo "Copying files for quantification"
+echo "Copying files for quantification..."
 bash src/pass_extract_atac_from_gcp.sh $NUM_CORES $ATAC_OUTPUT_DIR/croo $ATAC_OUTPUT_DIR/
 
 echo "Downloading and setting up gcsfuse..."
 wget -q https://github.com/GoogleCloudPlatform/gcsfuse/releases/download/v0.41.6/gcsfuse_0.41.6_amd64.deb
 sudo dpkg -i gcsfuse_0.41.6_amd64.deb
 
-echo "Mounting GCS bucket..."
-mkdir -p "$MOUNT_DIR"
-gcsfuse --implicit-dirs "$GCS_BUCKET" "$MOUNT_DIR"/"$GCS_BUCKET"
-
 # run samtools to generate genome alignment stats
-echo "Generating alignment stats"
-bash src/align_stats.sh $NUM_CORES ~/"$MOUNT_DIR"/${ATAC_OUTPUT_DIR#"gs://"} ~/"$MOUNT_DIR"/${ATAC_OUTPUT_DIR#"gs://"}/croo
+echo "Generating alignment stats..."
+bash src/align_stats.sh $NUM_CORES ~/"$MOUNT_DIR"/$LOCAL_ATAC_OUT_DIR ~/"$MOUNT_DIR"/$LOCAL_ATAC_OUT_DIR/croo
 
 # Create final qc report merging the metadata and workflow qc scores
-
-Rscript src/merge_atac_qc_human.R -w ~/${in_mnt_dir}/${sample_metadata} -q ~/${in_mnt_dir}/qc/${qc2tsv_report_name} -m ${in_mnt_dir}/rep_to_sample_map.csv -a ${in_mnt_dir}/merged_chr_info.csv -o ${in_mnt_dir}/
-
-echo "Success! Done generating merged qc reports"
+echo "Generating merged qc reports..."
+Rscript src/merge_atac_qc_human.R -w ~/"$MOUNT_DIR"/$LOCAL_ATAC_OUT_DIR/$SAMPLE_METADATA_FILENAME -q ~/"$MOUNT_DIR"/$LOCAL_ATAC_OUT_DIR/qc/"$BATCH_PREFIX"_qc.tsv -a ~/"$MOUNT_DIR"/$LOCAL_ATAC_OUT_DIR/merged_chr_info.csv -o ~/"$MOUNT_DIR"/${ATAC_OUTPUT_DIR#"gs://"}/
