b4afb3af22f5a64f412e29f6fbe6e2bbc26d4730
Nicole Gay, PhD <nicole.r.gay@gmail.com>
2023-10-11 20:51:33 -0700

    add tss_from_gtf.py

---
+++ src/tss_from_gtf.py
@@ -0,0 +1,47 @@
+import gzip
+import re
+import sys
+
+in_gtf = sys.argv[1]
+out_tss = sys.argv[2]
+
+# make TSS annotation file from GTF
+# chr1	69089	69090	ENSG00000186092.4	0	+
+# chr1    HAVANA  gene    11869   14409   .       +       .       gene_id "ENSG00000223972.5";
+
+with gzip.open(in_gtf, 'rb') as gtf, gzip.open(out_tss, 'wb') as out:
+
+	for line in gtf:
+
+		if line.startswith('#'):
+			continue
+
+		l = line.strip().split('\t')
+
+		if not l[2] == 'gene':
+			continue
+
+		strand = l[6]
+
+		if strand == '+':
+			tss = l[3]
+		elif strand == '-':
+			tss = l[4]
+		else:
+			print 'Strand not recognized'
+			break
+
+		chrom = l[0]
+
+		gene_id = l[8].split(';')[0].split(' ')[1]
+		gene_id = re.sub("\"", "", gene_id)
+
+		gene_type = l[8].split(';')[2].split(' ')[1]
+		if not gene_type == 'protein_coding':
+			continue
+
+		end = tss
+		start = int(tss) - 1
+
+		out.write('\t'.join([chrom, str(start), end, gene_id, '0', strand])+'\n')
+
